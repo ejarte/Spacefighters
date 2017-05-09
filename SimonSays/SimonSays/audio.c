75eaa75193276e95b773d6cbad60722b75fdcbdf
Farhad34 <guardianofthecity34@gmail.com>
2017-05-09 12:18:01 +0200

    sound edit

--- SimonSays/SimonSays/audio.c
+++ SimonSays/SimonSays/audio.c
@@ -28,7 +28,9 @@ Mix_Chunk *powerupSpeed;
 Mix_Chunk *powerupHP;
 Mix_Chunk *powerupAtk2;
 Mix_Chunk *powerupAtk3;
-Mix_Chunk *laser[4];				//för många??
+Mix_Chunk *laser[4];	//för många??
+
+
 
 void initAudio()
 {
